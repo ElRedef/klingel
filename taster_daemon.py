9dbd135d85ad1192e713d2347d1e6cbea3e7715e
johannes <johannes.Federle@gmail.com>
2020-06-06 09:58:43 +0200

    *klingel als programm nach allen versuchen dazu

---
+++ taster_daemon.py
@@ -0,0 +1,62 @@
+
+
+
+print("GPIO Test")
+
+
+from gpiozero import Button
+from time import sleep
+from picamera import PiCamera
+import http.client, urllib
+import requests
+import telegram_send
+
+
+USER_KEY = "ue1j2qp7uuinvrcuvmzztojobzv3re"
+APP_TOKEN= "asrgueig8omqmoh4zdvvxqt8kevs45"
+
+
+def date_time():
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y_%m_%d_%H_%M_%S_%f")
+    return date_time
+
+def send_pushover(message, img_path):
+
+  r = requests.post("https://api.pushover.net/1/messages.json", data = {
+    "token": APP_TOKEN,
+    "user": USER_KEY,
+    "message": message
+  },
+  files = {
+    "attachment": ("image.jpg", open(img_path, "rb"), "image/jpeg")
+  })
+  print(r.text)
+
+
+
+def send_telegram(message, img_path):
+    telegram_send.send(messages=[message])
+
+    with open(img_path, "rb") as f:
+        telegram_send.send(images=[f])
+
+
+
+button = Button(14)
+camera = PiCamera()
+
+while True:
+    if button.is_pressed:
+        print("Pressed")
+		
+		img_path='~/'
+		img_path+=''
+		img_path+=date_time()
+        img_path+= '.jpg'
+		
+        camera.capture(img_path)
+        m = "Es ist jemand an der Tür"
+        send_pushover(m,img_path)
+        send_telegram(m,img_path)
+    sleep(0.2)
---
+++ web_app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import send_from_directory
+from flask import render_template
+import datetime
+import os
+
+directory = 'X:\\pic'
+app = Flask(__name__, static_folder=directory) 
+
+
+def date_time():
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y_%m_%d_%H_%M_%S_%f")
+    return date_time
+
+
+def parse_dir():
+    events = []
+
+    for filename in os.listdir(directory):
+        a =filename.split(".") #trennt .jpg am Schluss ab
+        b=a[0].split("_")
+        try:
+            event = {'year':b[0],'month':b[1], 'day':b[2], 
+                'hour':b[3], 'min':b[4],'sec':b[5],'msec':b[6],
+                'filename':filename}
+            events.append(event)
+        except:
+            print("kann Datei nicht öffnen")
+    return events
+
+
+@app.route('/')
+def los():
+    return 'Hello Bild!'
+
+
+@app.route('/list')
+def list():
+    events = parse_dir()
+    return render_template('list.html', events=events)
+
+@app.route('/piclist')
+def piclist():
+    events = parse_dir()
+    return render_template('pic_list.html', events=events)
+
+@app.route('/<path:filename>')  
+def send_file(filename):  
+    return send_from_directory(app.static_folder, filename)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
