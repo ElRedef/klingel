f08a406987f5614db4ce2a16e5afec90823a370d
ElRedef <66745270+ElRedef@users.noreply.github.com>
2020-08-03 17:46:59 +0200

    *Livestreaming mit flask

--- klingel/live.py
+++ klingel/live.py
@@ -1,10 +1,11 @@
 
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory, Response
 )
 import os
 
+from camera.camera import Camera
 
 
 bp = Blueprint('live', __name__, url_prefix='/live')
@@ -16,6 +17,20 @@ def list():
     return render_template('live.html')
 
 
+#https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+#https://blog.miguelgrinberg.com/post/flask-video-streaming-revisited
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+@bp.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
