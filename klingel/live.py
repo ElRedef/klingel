7bab2834535df669c8d160ca9591ee10b906b9df
ElRedef <66745270+ElRedef@users.noreply.github.com>
2020-08-14 21:56:56 +0200

    Revert "Kann statische Bilder aufnehmen"
    This reverts commit 380f5c33d176153eac4c7fb26c4d77a99bfe53e1.
    
    Revert "Kamera funktioniert af Raspi"
    
    This reverts commit 085eb99ce71c3f2c40942a44f84971a5eca716d1.
    
    Revert "*Livestreaming mit flask"
    
    This reverts commit f08a406987f5614db4ce2a16e5afec90823a370d.
    
    Revert "* Camerafiles hinzu"
    
    This reverts commit 981a63d7cb520409477329c36f287dbd3ef34ce5.

--- klingel/live.py
+++ klingel/live.py
@@ -1,12 +1,10 @@
 
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory, Response, abort, current_app
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory
 )
 import os
 
-#from camera.camera import Camera
-from camera.camera_pi import Camera
 
 
 bp = Blueprint('live', __name__, url_prefix='/live')
@@ -18,26 +16,6 @@ def list():
     return render_template('live.html')
 
 
-#https://blog.miguelgrinberg.com/post/video-streaming-with-flask
-#https://blog.miguelgrinberg.com/post/flask-video-streaming-revisited
-
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@bp.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    
-@bp.route('/act_pic')
-def act_pic():
-    act_path = current_app.config['PIC_PATH']+ '//' +'actual.jpg'
-    print(act_path)
-    Camera.pic(act_path)
-    return send_from_directory(current_app.config['PIC_PATH'], 'act.jpg')  
