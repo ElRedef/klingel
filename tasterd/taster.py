dc2887e0fe542c0b271a205387210d72eccb980c
ElRedef <66745270+ElRedef@users.noreply.github.com>
2025-09-02 11:14:28 +0200

    Code clean Up

--- tasterd/taster.py
+++ tasterd/taster.py
@@ -19,101 +19,116 @@ from linphone import linphone
 class taster():
 
     #################################################################
-    #Konstruktor
+    # Konstruktor
     def __init__(self):
+        """
+        Initialisiert den Taster-Daemon:
+        - Lädt die Konfiguration
+        - Initialisiert den GPIO-Button
+        - Initialisiert die Telefonfunktion
+        """
         print("Klingel GPIO Daemon")
         self.loadconfig()
-        self.RASPI_BUTTON = Button(14)  # an diesen Port ist der Taster angeschlossen
-         
-        #Einstellungen
-        #self.PUSHOVER_USER_KEY = "ue1j2qp7uuinvrcuvmzztojobzv3re"
-        #self.PUSHOVER_APP_TOKEN= "asrgueig8omqmoh4zdvvxqt8kevs45"
-        #self.MESSAGE = "Es ist jemand an der Tür"  #Diese Nachricht wird gesendet
-        #self.PIC_URL = 'http://raspi:8080/?action=snapshot' #URL MJPEG Streamer um Bild abzuholen
-        #self.PIC_SOURCE = "streamer"   #Gibt an wie das Bild aufgenomment wird: "streamer" oder "camera"
-        
+        self.RASPI_BUTTON = Button(14)  # GPIO-Port für den Taster
+        # Initialisiere Linphone mit Konfigurationsdaten
         self.phone = linphone()
-        self.phone.ini(self.config.PHONE_HOST,self.config.PHONE_USER,self.config.PHONE_PW)
+        self.phone.ini(self.config.PHONE_HOST, self.config.PHONE_USER, self.config.PHONE_PW)
 
         
  
  
  
     #################################################################
-    #Lädt die Config aus dem JSON File
+    # Lädt die Config aus dem JSON File
     def loadconfig(self):
+        """
+        Lädt die Einstellungen aus der JSON-Konfigurationsdatei.
+        Falls die Umgebungsvariable nicht gesetzt ist, wird ein Standardpfad verwendet.
+        """
         self.settings_available = False
-    
         try:
             settings_file = os.environ['KLINGEL_SETTING_FILE']
-            print("Reading Settings from: "+settings_file)
+            print(f"Reading Settings from: {settings_file}")
         except:
             print("Cannot read environment variable: KLINGEL_SETTING_FILE")
             print("using default: /home/pi/hausautomatisierung/klingel/config.json")
             settings_file = '/home/pi/hausautomatisierung/klingel/config.json'
-
-        # create config parser
+        # Konfigurationsparser erstellen
         builder = ConfigBuilder()
-
-        # parse config
         try:
-            self.config = builder.parse_config(settings_file)    
-        except: 
-            print("Cannot read setting file: " + settings_file)
+            self.config = builder.parse_config(settings_file)
+        except:
+            print(f"Cannot read setting file: {settings_file}")
             return
-    
         self.settings_available = True
 
         
     #################################################################
-    #Gibt einen String zurueck der das Datum wiederspiegelt
+    # Gibt einen String zurück, der das aktuelle Datum und die Uhrzeit enthält
     def date_time(self):
+        """
+        Gibt das aktuelle Datum und die Uhrzeit als formatierten String zurück.
+        Format: YYYY_MM_DD_HH_MM_SS_microseconds
+        """
         now = datetime.datetime.now()
         date_time = now.strftime("%Y_%m_%d_%H_%M_%S_%f")
         return date_time
     
     
     #################################################################
-    #Sendet ueber Pushover
-    def send_pushover(self,message, img_path):
-      r = requests.post("https://api.pushover.net/1/messages.json", data = {
-        "token": self.config.PUSHOVER_APP_TOKEN,
-        "user": self.config.PUSHOVER_USER_KEY,
-        "message": message
-      },
-      files = {
-        "attachment": ("image.jpg", open(img_path, "rb"), "image/jpeg")
-      })
-      print("Pushover: "+ r.text)
+    # Sendet eine Benachrichtigung über Pushover
+    def send_pushover(self, message, img_path):
+        """
+        Sendet eine Nachricht und ein Bild über den Pushover-Dienst.
+        """
+        r = requests.post(
+            "https://api.pushover.net/1/messages.json",
+            data={
+                "token": self.config.PUSHOVER_APP_TOKEN,
+                "user": self.config.PUSHOVER_USER_KEY,
+                "message": message
+            },
+            files={
+                "attachment": ("image.jpg", open(img_path, "rb"), "image/jpeg")
+            }
+        )
+        print("Pushover: " + r.text)
 
       
     #################################################################
-    #Sendet ueber Telegram
-    def send_telegram(self,message, img_path):
+    # Sendet eine Nachricht und ein Bild über Telegram
+    def send_telegram(self, message, img_path):
+        """
+        Sendet eine Textnachricht und ein Bild über Telegram.
+        """
         telegram_send.send(messages=[message])
-
         with open(img_path, "rb") as f:
             telegram_send.send(images=[f])
 
             
     #################################################################
-    #Macht ein Bild
-    #Uber Type kann eingestellt werden ob dies über eine URL
-    #Oder über die RaspiKamera gemacht wird
-    #TODO: Fehler abfangen wenn z.b. URL nicht erreicht werden kann
-    def capture_pic(self,type,dest):
-        if type=="camera":
+    # Erstellt ein Bild
+    # Über "type" kann eingestellt werden, ob das Bild über die Kamera oder eine URL aufgenommen wird
+    # TODO: Fehler abfangen, wenn z.B. die URL nicht erreichbar ist
+    def capture_pic(self, type, dest):
+        """
+        Erstellt ein Bild und speichert es unter "dest".
+        Typen:
+        - "camera": Aufnahme mit PiCamera
+        - "streamer": Download über MJPEG-Streamer-URL
+        """
+        if type == "camera":
             camera = PiCamera()
             try:
-                camera.capture(dest)  
+                camera.capture(dest)
             except:
-                #print("Failed to make image via " + PIC_URL)
+                # Fehler beim Aufnehmen mit Kamera
                 src = self.config.path + "/no_pic.jpg"
             finally:
                 camera.close()
         if type == "streamer":
             try:
-                urllib.request.urlretrieve(self.config.PIC_URL,dest)
+                urllib.request.urlretrieve(self.config.PIC_URL, dest)
             except:
                 print("Failed to make image via " + self.config.PIC_URL)
                 src = self.config.no_path + "no_pic.jpg"
@@ -122,38 +137,47 @@ class taster():
        
                 
     #################################################################
+    # Hauptschleife: Überwacht den Taster und führt Aktionen aus
     def run(self):
+        """
+        Endlosschleife, die den GPIO-Taster überwacht.
+        Bei Tastendruck werden folgende Aktionen ausgeführt:
+        - Sound abspielen
+        - Bild aufnehmen
+        - (optional) Benachrichtigung versenden
+        - Telefonanruf auslösen
+        """
         while True:
             if self.RASPI_BUTTON.is_pressed:
-            #if 1:
                 print("Pressed")
-                os.system("aplay " +self.config.SOUNDFILE + "&")
-                img_path=self.config.image_path +"/"+ self.date_time() + '.jpg'
-                self.capture_pic(self.config.PIC_SOURCE,img_path)
-                #self.send_pushover(self.config.MESSAGE,img_path)
-                #self.send_telegram(self.config.MESSAGE,img_path)
-             
-             
+                # Klingelton abspielen
+                os.system("aplay " + self.config.SOUNDFILE + "&")
+                # Bild aufnehmen und speichern
+                img_path = self.config.image_path + "/" + self.date_time() + '.jpg'
+                self.capture_pic(self.config.PIC_SOURCE, img_path)
+                # Benachrichtigungen (optional, auskommentiert)
+                #self.send_pushover(self.config.MESSAGE, img_path)
+                #self.send_telegram(self.config.MESSAGE, img_path)
+                # Telefonstatus prüfen und Anruf auslösen
                 status = self.phone.get_register_status()
                 print("Linphone status: " + status)
                 self.phone.dial(self.config.PHONE_NUMBER)
                 self.phone.wait_for_call(self.config.PHONE_CALL_TIME)
-                
-                #sleep(3) #verhindert Sturmklingeln
+                #sleep(3) # verhindert Sturmklingeln
             sleep(0.1)
 
     
     
 #################################################################
-if __name__ == "__main__":  
-
+# Einstiegspunkt für das Skript
+if __name__ == "__main__":
+    # Signal-Handler für sauberes Beenden mit Ctrl+C
     def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
         sys.exit(0)
-    signal.signal(signal.SIGINT, signal_handler) 
+    signal.signal(signal.SIGINT, signal_handler)
 
     print("Taster: Start watching")
- 
     myApp = taster()
     if myApp.settings_available:
         myApp.run()
