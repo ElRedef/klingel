8e0da72ad32768bfcc7b65e6acbcd6405e7b1b25
ElRedef <66745270+ElRedef@users.noreply.github.com>
2025-09-02 11:06:57 +0200

    Clean Up

--- start.ps1
+++ start.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Host "Running Flask"
 $env:FLASK_APP = "klingel"
 $env:FLASK_ENV = "development"
-$env:KLINGEL_SETTING_FILE = "C:\Users\Johannes\Programming\hausautomatisierung\klingel\configwin.json"
+$env:KLINGEL_SETTING_FILE = "C:\Users\xxx\Programming\hausautomatisierung\klingel\configwin.json"
 
 
 #set FLASK_APP=klingel
